117aba299f4b76b52e33ee7cdc08f3e59e882e01
GuestAlexander <119532414+GuestAlexander@users.noreply.github.com>
2023-04-11 22:49:29 -0400

    newnamez

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Welcome to KrunchyMa Pedia</h1>
+      <h1>Welcome to KrunchyMom.com </h1>
       <p>This is a platform for sharing knowledge and information.</p>
     </div>
   );
