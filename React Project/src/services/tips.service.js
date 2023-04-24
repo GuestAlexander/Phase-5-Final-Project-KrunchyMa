733f7dd7f3c6329ad13ec5b7e9f3dbc3ebac89ea
GuestAlexander <119532414+GuestAlexander@users.noreply.github.com>
2023-04-23 19:03:46 -0600

    Update tips.service.js

--- React Project/src/services/tips.service.js
+++ React Project/src/services/tips.service.js
@@ -10,7 +10,7 @@ const postTips = (desc) => {
     return axios.post(API_URL + "add", {desc}, {headers: authHeader()});
 };
 
-const getAllTips= () => {
+const getAllTips = () => {
     return axios.get(API_URL + "get", {headers: authHeader()});
 };
 
@@ -18,17 +18,23 @@ const getByIdTips = (id) => {
     return axios.get(API_URL + `get/${id}`, {headers: authHeader()});
 };
 
-const updateTips = (id,desc) => {
+const updateTips = (id, desc) => {
     return axios.put(API_URL + `update/${id}`, {desc}, {headers: authHeader()});
 };
 
+const deleteTips = (id) => {
+    return axios.delete(API_URL + `delete/${id}`, {headers: authHeader()});
+};
+
 const getMyTips = () => {
-    return axios.get(API_USER_URL ,{headers: authHeader()});
+    return axios.get(API_USER_URL, {headers: authHeader()});
 };
 
 export default {
     postTips,
     getAllTips,
     getByIdTips,
-    updateTips,getMyTips
+    updateTips,
+    deleteTips,
+    getMyTips
 };
